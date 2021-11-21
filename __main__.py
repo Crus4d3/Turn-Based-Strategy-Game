07a3668263cc91c060399e2f4c5e931141933b1e
Crus4d3 <matti.armston@gmail.com>
2021-11-21 17:41:18 +0000

    Removed use of globals
    I removed the use of globals but had to hack in a change for the
    mouseClick class.
    I may end up removing the class entirely if I can come up with a
    better solution, but for now it's hacked in.
    If I can find a way that doesn't use the class or doesn't run into the
    current chicken and the egg problem that'd be great and what I switch to.

--- __main__.py
+++ __main__.py
@@ -9,11 +9,11 @@ class Window():
         #Setting up window
         self.WIDTH = 1000
         self.HEIGHT = 1000
-        self.WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.WINDOW = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Turn based strategy game")
 
 class Assets():
-    def __init__(self):
+    def __init__(self, window):
         # Setting up images
         self.RedCircleImage = pygame.image.load(
             os.path.join(
@@ -28,7 +28,7 @@ class Assets():
                     "BGImage.png"
                 )
             ),
-            (WIDTH, HEIGHT)
+            (window.WIDTH, window.HEIGHT)
         )
         self.BaseImage = pygame.image.load(
             os.path.join(
@@ -37,38 +37,39 @@ class Assets():
             )
         )
 
-#Setting up window
-WIDTH, HEIGHT = 1000, 1000
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Turn based strategy game")
-
-# Setting up images
-RedCircleImage = pygame.image.load(
-    os.path.join(
-        "assets",
-        "RedCircleImage.png"
-    )
-)
-BGImage = pygame.transform.scale(
-    pygame.image.load(
-        os.path.join(
-            "assets",
-            "BGImage.png"
-        )
-    ),
-    (WIDTH, HEIGHT)
-)
-BaseImage = pygame.image.load(
-    os.path.join(
-        "assets",
-        "BaseImage.png"
-    )
-)
+# #Setting up window
+# wIDTH, HEIGHT = 1000, 1000
+# wINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+# pygame.display.set_caption("Turn based strategy game")
+# 
+# # Setting up images
+# redCircleImage = pygame.image.load(
+#     os.path.join(
+#         "assets",
+#         "RedCircleImage.png"
+#     )
+# )
+# bGImage = pygame.transform.scale(
+#     pygame.image.load(
+#         os.path.join(
+#             "assets",
+#             "BGImage.png"
+#         )
+#     ),
+#     (WIDTH, HEIGHT)
+# )
+# BaseImage = pygame.image.load(
+#     os.path.join(
+#         "assets",
+#         "BaseImage.png"
+#     )
+# )
 
 class Game:
-    def __init__(self, window, assets):
+    def __init__(self, window, assets, mouseClick):
         self.window = window
         self.assets = assets
+        self.mouseClick = mouseClick
         self.run = True
         self.lost = False
         self.toDraw= []
@@ -89,8 +90,8 @@ class Game:
 
     def main(self):
         self.takeInputs()
-        mouseClick.moveToMouseClick()
-        mouseClick.itemsClicked()
+        self.mouseClick.moveToMouseClick()
+        self.mouseClick.itemsClicked(self.toDraw)
         self.drawFrame()
 
     def mainMenu(self):
@@ -99,38 +100,38 @@ class Game:
             self.main()
 
 class GameObject:
-    def __init__(self, x, y, width, height, image):
+    def __init__(self, x, y, width, height, image, game):
         self.x = int(x)
         self.y = int(y)
         self.width = width
         self.height = height
         self.image = pygame.transform.scale(image, (self.width, self.height))
+        self.game = game
 
     def onClick(self):
         pass
 
 class InfoBox(GameObject):
-    def __init__(self, x, y, message):
+    def __init__(self, x, y, message, game):
         self.image = game.infoFont.render(message, 1, (255, 255, 255)) 
-        super().__init__(x, y, self.image.get_width(),
-                         self.image.get_height(), self.image)
+        super().__init__(x, y, self.image.get_width(), self.image.get_height(), self.image, game)
         self.mask = pygame.mask.from_surface(self.image)
         game.toDraw.append(self)
 
 class Base(GameObject):
-    def __init__(self, x, y, width, height, image):
-        super().__init__(x, y ,width, height, image)
+    def __init__(self, x, y, width, height, assets, game):
+        super().__init__(x, y ,width, height, assets.BaseImage, game)
         self.mask = pygame.mask.from_surface(self.image)
 
     def onClick(self):
         self.info()
 
     def info(self):
-        infoBox = InfoBox(self.x, self.y - self.height, "This is your base!")
+        infoBox = InfoBox(self.x, self.y - self.height, "This is your base!", self.game)
 
 class MouseClick(GameObject):
-    def __init__(self, x, y, width, height, image):
-        super().__init__(x, y, width, height, image)
+    def __init__(self, x, y, width, height, assets):
+        super().__init__(x, y, width, height, assets.RedCircleImage, "Haha this is a bad hack deal with it!")
         self.x = int(self.x - self.width/2)
         self.y = int(self.y - self.height/2)
         self.mask = pygame.mask.from_surface(self.image)
@@ -140,18 +141,23 @@ class MouseClick(GameObject):
             mouseX, mouseY = pygame.mouse.get_pos()
             self.x, self.y = int(mouseX - self.width/2), int(mouseY - self.height/2)
 
-    def itemsClicked(self):
-        for item in game.toDraw:
+    def itemsClicked(self, toDraw):
+        for item in toDraw:
             overlapX = item.x - self.x
             overlapY = item.y - self.y
             if self.mask.overlap(item.mask, (overlapX, overlapY)) and type(item) != type(self):
                 item.onClick()
 
-window = Window()
-assets = Assets()
-game = Game(window, assets)
-mouseClick = MouseClick(-10, -10, 15, 15, RedCircleImage)
-base = Base(WIDTH/2, HEIGHT/2, 50, 50, BaseImage)
-game.toDraw.append(mouseClick)
-game.toDraw.append(base)
-game.mainMenu()
+def main():
+    # Creating instances
+    window = Window()
+    assets = Assets(window)
+    mouseClick = MouseClick(-10, -10, 15, 15, assets)
+    game = Game(window, assets, mouseClick)
+    base = Base(window.WIDTH/2, window.HEIGHT/2, 50, 50, assets, game)
+    # Starting game
+    game.toDraw = [mouseClick, base]
+    game.mainMenu()
+
+if __name__ == "__main__":
+    main()
